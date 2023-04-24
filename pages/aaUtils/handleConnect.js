871bd7771ab5ef9b4a1ba6a52af99def08758d3e
bcfh <crunchy.wafer.stick787@gmail.com>
2023-04-24 13:57:42 +0800

    add connect with web3Auth

--- pages/aaUtils/handleConnect.js
+++ pages/aaUtils/handleConnect.js
@@ -1,7 +1,25 @@
 import { getWebSimpleAccount } from './getWebSimpleAccount';
 import { config } from './config';
+import { Web3Auth } from "@web3auth/modal";
+import { useEffect, useState } from "react";
 
 export const handleConnect = async () => {
+
+
+
+  //Initialize within your constructor
+  // const web3auth = new Web3Auth({
+  //   clientId: "BKP4fsLio7EVgA2GH0iQGUWfZCQaroScJWDQhRxMK3beDhU7beudVK3-F_opZ3z3nqaESbzMDFUsHDhZ8-MMvZ4", // Get your Client ID from Web3Auth Dashboard
+  //   chainConfig: {
+  //     chainNamespace: "eip155",
+  //     chainId: "0x539",
+  //   },
+  // });
+  // console.log("web3auth", web3auth);
+
+  // await web3auth.initModal();
+  // await web3auth.connect();
+  // const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
   // debugger
   console.log(config);
   // debugger;
